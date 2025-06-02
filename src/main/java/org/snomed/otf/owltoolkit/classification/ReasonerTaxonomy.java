1b5836cf9da79bb5c018b2ae2fa422813bc6201f
b2i-ci <ci@b2international.com>
2025-06-02 10:25:30 +0000

    chore: update license headers

--- src/main/java/org/snomed/otf/owltoolkit/classification/ReasonerTaxonomy.java
+++ src/main/java/org/snomed/otf/owltoolkit/classification/ReasonerTaxonomy.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2018 SNOMED International, http://snomed.org
- * Copyright 2011-2016 B2i Healthcare Pte Ltd, http://b2i.sg
+ * Copyright 2011-2016 B2i Healthcare, https://b2ihealthcare.com
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
